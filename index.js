a16a4ddedb1dfcb9ff75adb6d3cd07f9fb6f0727
wreising <wreising@mac.com>
2022-03-11 10:04:11 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const gemerateMarkdown = require('generateMarkdown.js')
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ]);
+};
+
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
