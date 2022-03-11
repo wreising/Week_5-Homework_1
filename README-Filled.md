8ceaa7707d7e088a2eeb558e91451142f4df95a9
wreising <wreising@mac.com>
2022-03-11 12:30:41 -0800

    first run

---
+++ README-Filled.md
@@ -0,0 +1,23 @@
+# README
+# Homework
+### Bill
+
+## Description
+
+do homework
+
+## Installation Instructions
+
+node index.js
+
+## Usage Instructions
+
+node
+
+## Contribution Guidlines
+
+don't be evil
+
+## License: href=Apache 2.0
+
+## Questions: https://github.com/wreising
--- index.js
+++ index.js
@@ -60,42 +60,67 @@ const promptUser = () => {
   ]);
 };
 
-const generateHTML = ({ name, title, description, installInstructions, usageInstructions, contributionGuidlines, license, gitHubUserName }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1>${title}</h1>
-    <p> Hello, my name is ${name} and this is my Project.</p>
-    <h2>Project Description:</h2>
-    <p>${description}</p>
-    <h2>Installation Instructions:</h2>
-    <p>${installInstructions}</p>
-    <h2>Usage Instructions:</h2>
-    <p>${usageInstructions}</p>
-    <h2>Contribution Guidlines</h2>
-    <p>${contributionGuidlines}</p>
-    <h3>License: ${license}</h3>
-    <h2>Questions:</h2>
-    <p>${gitHubUserName}</p>
-  </div>
-</div>
-</body>
-</html>`;
+const generateMD = ({ name, title, description, installInstructions, usageInstructions, contributionGuidlines, license, gitHubUserName }) =>
+  `# README
+# ${title}
+### ${name}
+
+## Description
+
+${description}
+
+## Installation Instructions
+
+${installInstructions}
+
+## Usage Instructions
+
+${usageInstructions}
+
+## Contribution Guidlines
+
+${contributionGuidlines}
+
+## License: href=${license}
+
+## Questions: https://github.com/${gitHubUserName}`
+
+// const generateHTML = ({ name, title, description, installInstructions, usageInstructions, contributionGuidlines, license, gitHubUserName }) =>
+//   `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1>${title}</h1>
+//     <p> Hello, my name is ${name} and this is my Project.</p>
+//     <h2>Project Description:</h2>
+//     <p>${description}</p>
+//     <h2>Installation Instructions:</h2>
+//     <p>${installInstructions}</p>
+//     <h2>Usage Instructions:</h2>
+//     <p>${usageInstructions}</p>
+//     <h2>Contribution Guidlines</h2>
+//     <p>${contributionGuidlines}</p>
+//     <h3>License: ${license}</h3>
+//     <h2>Questions:</h2>
+//     <p>GitHub: https://github.com/${gitHubUserName}</p>
+//   </div>
+// </div>
+// </body>
+// </html>`;
 
 // Bonus using writeFileSync as a promise
 const init = () => {
   promptUser()
     // Use writeFileSync method to use promises instead of a callback function
-    .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
+    .then((answers) => fs.writeFileSync('README-Filled.md', generateMD(answers)))
+    .then(() => console.log('Successfully wrote to README-Filled.md'))
     .catch((err) => console.error(err));
 };
 
